3334876b9dfe6c7608ab48d30e6e4a47b1717378
andre <andre@LAPTOP-FR1TKDQC>
2018-02-12 08:38:16 -0500

    1.0

--- src/unal/poo/practica/RobotBase.java
+++ src/unal/poo/practica/RobotBase.java
@@ -12,49 +12,38 @@ public class RobotBase
         public static City objetos;
         public static Robot estudiante;
         
+        public static boolean rightclear (){
+                turn(3);
+                boolean ret = estudiante.frontIsClear();
+                turn(1);
+                return ret;
+            }
+            
+        public static void turn ( int x ){
+            for( int i = 0 ; i < x ; i ++ )
+                estudiante.turnLeft();
+        }
+        
 	public static void main (String[] args){
             //Declarar la creacion de la ciudad
             objetos = new City("Field.txt");
 	    objetos.showThingCounts(true);
             
+            
             //Direction.NORTH, EAST, SOUTH, WEST
             //Definicion de la ubicacion del robot, Ciudad, posicion, Direccion, Numero things en el bolso.
-            estudiante = new Robot(objetos,0, 2, Direction.EAST,10);
+            estudiante = new Robot(objetos,10, 1, Direction.EAST,1000);
+               
+            while( rightclear() ) turn(1);
             
-	    //Mover una interseccion en el sentido al cual este apuntando el objeto.
-            estudiante.move ();
-            
-            //Girar a la izquierda
-            estudiante.turnLeft();
-            
-            //Tomando decisiones, Si puedo tomar un Thing
-            boolean puedeTomar = estudiante.canPickThing();
-            
-            //Tomar un Thing
-            if(puedeTomar == true)
-               estudiante.pickThing();
-            
-            //Especifica el numero de Thing que tiene en robot en el bolso
-            int numeroThings = estudiante.countThingsInBackpack();
-            
-            //Poner Thing, se debe validar que tenga things en el bolso
-            estudiante.putThing();
-                       
-            //Si el frente esta libre de Wall
-            estudiante.frontIsClear();
-            
-            //Invocando una funcion
-            creacionFuncion(4);
-            
-            //Toman un Thing
+            while( !estudiante.canPickThing() ){
+                if( rightclear() ){
+                    turn(3);
+                    estudiante.move();
+                }else if( estudiante.frontIsClear() ) estudiante.move();
+                else turn(1);
+            }
             estudiante.pickThing();
-            
-            
-	}
-        
-        public static void creacionFuncion(int parametroEntrada){
-            for (int i = 0; i < parametroEntrada; i++) 
-                estudiante.move();
         }
+            
 }
-
